0570e42f918f620751613fba5a2f46a0a98f755f
Kumari Khushi <flourishingflaws06@gmail.com>
2026-08-05 13:59:49 +0530

    Change test assertion to fail

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 // A deliberately passing test to start with — we'll break this on demand later
 const assert = require("assert");
-assert.strictEqual(1 + 1, 2);
+assert.strictEqual(1 + 1, 3);
 console.log("test passed");
